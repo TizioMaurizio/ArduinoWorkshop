db5a5b84bc23e67300ea1204126e7f7060835863
TizioMaurizio <maurizio.vetere@mail.polimi.it>
2020-05-08 01:26:18 +0200

    Fixed major bug on the mqtt car's hand

--- Projects/MQTT_RC_Car_Arm/MQTT_RC_Car_Arm.ino
+++ Projects/MQTT_RC_Car_Arm/MQTT_RC_Car_Arm.ino
@@ -137,22 +137,25 @@ void showNewData() {
          //ROTATE
          //start 90, max 150, min 30
          if(receivedChar == '0'){ 
-          if(rotate > 30)
-            pwm.setPWM(14, 0, angleToPulse(rotate-=20) ); 
+          if(rotate > 10)
+            pwm.setPWM(14, 0, angleToPulse(rotate-=20) );
+          Serial.println(rotate);
          }
          if(receivedChar == '1'){
-          if(rotate < 150)
+          if(rotate < 170)
             pwm.setPWM(14, 0, angleToPulse(rotate+=20) );
+          Serial.println(rotate);
          }
 
          //JOINT 1
          //start 90, max 105, min 15
          if(receivedChar == '2'){
-          if(joint1 > 15){
+          if(joint1 > 0){
             joint1-=15;
             pwm.setPWM(7, 0, angleToPulse(joint1) );
             pwm.setPWM(15, 0, angleToPulse(180-joint1) );
           }
+          Serial.println(joint1);
          }
          if(receivedChar == '3'){
           if(joint1 < 105){
@@ -160,6 +163,7 @@ void showNewData() {
             pwm.setPWM(7, 0, angleToPulse(joint1) );
             pwm.setPWM(15, 0, angleToPulse(180-joint1) );
           }
+          Serial.println(joint1);
          }
 
          //JOINT 2
@@ -167,10 +171,12 @@ void showNewData() {
          if(receivedChar == '4'){
           if(joint2 > 0)
             pwm.setPWM(13, 0, angleToPulse(joint2-=15) );
+          Serial.println(joint2);
          }
          if(receivedChar == '5'){
           if(joint2 < 240)
-            pwm.setPWM(13, 0, angleToPulse(joint2+=15) ); 
+            pwm.setPWM(13, 0, angleToPulse(joint2+=15) );
+          Serial.println(joint2); 
          }
          
          //HAND
@@ -178,10 +184,12 @@ void showNewData() {
          if(receivedChar == '6'){ 
           if(hand > 100)
             pwm.setPWM(12, 0, angleToPulse(hand-=10) ); //OPEN
+          Serial.println(hand);
          }
          if(receivedChar == '7'){
           if(hand < 180)
             pwm.setPWM(12, 0, angleToPulse(hand+=10) ); //CLOSE
+          Serial.println(hand);
          }
          
          //CAMERA VERTICAL
@@ -189,10 +197,12 @@ void showNewData() {
          if(receivedChar == 'm'){
           if(camVer > 70)
             pwm.setPWM(0, 0, angleToPulse(camVer-=10) ); //UP
+          Serial.println(camVer);
          }
          if(receivedChar == 'n'){
           if(camVer < 230)
             pwm.setPWM(0, 0, angleToPulse(camVer+=10) ); //DOWN
+          Serial.println(camVer);
          }
          
          //CAMERA HORIZONTAL
@@ -200,19 +210,21 @@ void showNewData() {
          if(receivedChar == 'j'){ 
           if(camHor > 150)
             pwm.setPWM(1, 0, angleToPulse(camHor-=10) ); //LEFT
+          Serial.println(camHor);
          }
          if(receivedChar == 'k'){
           if(camHor < 250)
             pwm.setPWM(1, 0, angleToPulse(camHor+=10) ); //RIGHT
+          Serial.println(camHor);
          }
 
          //UNUSED
-         if(receivedChar == 'y'){
+         /*if(receivedChar == 'y'){
           pwm.setPWM(10, 0, angleToPulse(hand-=15) );
          }
-         if(receivedChar == 'u'){
+         if(receivedChar == 'y'){
           pwm.setPWM(10, 0, angleToPulse(hand+=15) );
-         }
+         }*/
  newData = false;
  }
 }
