f6debb30aaf4c02ea35da99d8f3f116349bd7a3e
TizioMaurizio <maurizio.vetere@mail.polimi.it>
2020-05-21 19:35:58 +0200

    RC car infrared mode added

--- Projects/ESP32-CAM/CameraMqttServer/CameraMqttServer.ino
+++ Projects/ESP32-CAM/CameraMqttServer/CameraMqttServer.ino
@@ -1,8 +1,13 @@
 #include "esp_camera.h"
 #include <WiFi.h>
 #include <ArduinoMqttClient.h>
+#include "BluetoothSerial.h"
 #include "soc/soc.h" //disable brownout problems
 #include "soc/rtc_cntl_reg.h"  //disable brownout problems
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
 //
 // WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
@@ -16,12 +21,14 @@
 //#define CAMERA_MODEL_M5STACK_WIDE
 #define CAMERA_MODEL_AI_THINKER
 #define LED_BUILTIN 4
+#define WIFI false
 
 #include "camera_pins.h"
 
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
+BluetoothSerial SerialBT;
 const char broker[] = "192.168.43.1";
 int        port     = 1883;
 const char topic[]  = "scan";
@@ -38,146 +45,167 @@ void setup() {
   Serial.setDebugOutput(true);
   Serial.print("&à");
   Serial.println();
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-
-  sensor_t * s = esp_camera_sensor_get();
-  //initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);//flip it back
-    s->set_brightness(s, 1);//up the blightness just a bit
-    s->set_saturation(s, -2);//lower the saturation
-  }
-  //drop down frame size for higher initial frame rate
-  s->set_framesize(s, FRAMESIZE_QVGA);
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-  /*Serial.println("Insert wifi name");
-  char* wifiname;
-  while(!Serial.available()){
+  if(WIFI){
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    //init with high specs to pre-allocate larger buffers
+    if(psramFound()){
+      config.frame_size = FRAMESIZE_UXGA;
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+    } else {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.jpeg_quality = 12;
+      config.fb_count = 1;
+    }
+  
+  #if defined(CAMERA_MODEL_ESP_EYE)
+    pinMode(13, INPUT_PULLUP);
+    pinMode(14, INPUT_PULLUP);
+  #endif
+  
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+      Serial.printf("Camera init failed with error 0x%x", err);
+      return;
+    }
+    Serial.printf("Camera ready\n");
+    sensor_t * s = esp_camera_sensor_get();
+    //initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID) {
+      s->set_vflip(s, 1);//flip it back
+      s->set_brightness(s, 1);//up the blightness just a bit
+      s->set_saturation(s, -2);//lower the saturation
+    }
+    //drop down frame size for higher initial frame rate
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  
+  #if defined(CAMERA_MODEL_M5STACK_WIDE)
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+  #endif
+    /*Serial.println("Insert wifi name");
+    char* wifiname;
+    while(!Serial.available()){
+      delay(300);
+    }
+    Serial.readString().toCharArray(wifiname, 20);
+    Serial.println("Insert wifi password");
+    char* wifipassword;
+    while(!Serial.available()){
+      delay(300);
+    }
+    Serial.readString().toCharArray(wifipassword, 20);*/
+    
+    Serial.printf("Turn on WiFi...\n");
+    WiFi.enableSTA(true);
     delay(300);
-  }
-  Serial.readString().toCharArray(wifiname, 20);
-  Serial.println("Insert wifi password");
-  char* wifipassword;
-  while(!Serial.available()){
+    WiFi.begin(ssid, password);
     delay(300);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("&è");
+    Serial.print("Attempting to connect to the MQTT broker: ");
+    Serial.println(broker);
+  
+    if (!mqttClient.connect(broker, port)) {
+      Serial.print("MQTT connection failed! Error code = ");
+      Serial.println(mqttClient.connectError());
+  
+      while (1);
+    }
+  
+    Serial.println("You're connected to the MQTT broker!");
+    Serial.println();
+  
+    Serial.print("Subscribing to topic: ");
+    Serial.println(topic);
+    Serial.println();
+  
+    // subscribe to a topic
+    mqttClient.subscribe(topic);
+  
+    // topics can be unsubscribed using:
+    // mqttClient.unsubscribe(topic);
+  
+    Serial.print("Waiting for messages on topic: ");
+    Serial.println(topic);
+    Serial.println();
+  
+    delay(3000);
+    startCameraServer();
+  
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+    mqttClient.beginMessage(topic);
+    mqttClient.print(WiFi.localIP());
+    mqttClient.endMessage();
+    Serial.print("&ì");
+    digitalWrite(4, HIGH);
   }
-  Serial.readString().toCharArray(wifipassword, 20);*/
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  else{ //bluetooth mode
+    Serial.printf("Turn on Bluetooth...\n");
+    Serial.begin(115200);
+    SerialBT.begin("ESP32test"); //Bluetooth device name
+    Serial.println("The device started, now you can pair it with bluetooth!");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("&è");
-  Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.println(broker);
-
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1);
-  }
-
-  Serial.println("You're connected to the MQTT broker!");
-  Serial.println();
-
-  Serial.print("Subscribing to topic: ");
-  Serial.println(topic);
-  Serial.println();
-
-  // subscribe to a topic
-  mqttClient.subscribe(topic);
-
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
-
-  Serial.print("Waiting for messages on topic: ");
-  Serial.println(topic);
-  Serial.println();
-
-  delay(3000);
-  startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-  mqttClient.beginMessage(topic);
-  mqttClient.print(WiFi.localIP());
-  mqttClient.endMessage();
-  Serial.print("&ì");
-  digitalWrite(4, HIGH);
 }
 
 boolean light = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int messageSize = mqttClient.parseMessage();
-  if (messageSize) {
-    // we received a message, print out the topic and contents
-    // use the Stream interface to print the contents
-    while (mqttClient.available()) {
-      char received = (char)mqttClient.read();
-      Serial.print(received);
-      /*if (received == ','){
-        light = !light;
-        if(light)
-          digitalWrite(LED_BUILTIN, HIGH);
-        else
-          digitalWrite(LED_BUILTIN, LOW);
-      }*/
+  if (WIFI){
+    int messageSize = mqttClient.parseMessage();
+    if (messageSize) {
+      // we received a message, print out the topic and contents
+      // use the Stream interface to print the contents
+      while (mqttClient.available()) {
+        char received = (char)mqttClient.read();
+        Serial.print(received);
+        if (received == ','){
+          light = !light;
+          if(light)
+            digitalWrite(LED_BUILTIN, HIGH);
+          else
+            digitalWrite(LED_BUILTIN, LOW);
+        }
+      }
     }
+    delay(10);
   }
-  delay(10);
+  else{
+    if (Serial.available()) {
+      SerialBT.write(Serial.read());
+    }
+    if (SerialBT.available()) {
+      Serial.write(SerialBT.read());
+    }
+    delay(20);
+  }
 }
